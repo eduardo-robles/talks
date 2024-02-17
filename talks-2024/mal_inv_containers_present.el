75df1b8995cb2ccaeb24c04ff7805d9111fc40fe
Eduard Robles <eduardorobles@protonmail.com>
2024-02-16 22:13:23 -0600

    ADD: Updating talk on malware investigation

--- talks-2024/mal_inv_containers_present.el
+++ talks-2024/mal_inv_containers_present.el
@@ -1,4 +1,15 @@
+;;; Malicious Links Investigation - Demo
+;;
+;; Author: Eduardo Robles <eduardorobles@proton.me>
+;;
+;;; Code:
 (require 'demo-it)
 
 (demo-it-create :windows-below :fullscreen :advance-mode :use-shell :variable-width :insert-fast :text-large
 		(demo-it-presentation "mal_inv_containers.org")
+		demo-it-presentation-advance
+		)
+
+
+
+(demo-it-start)
