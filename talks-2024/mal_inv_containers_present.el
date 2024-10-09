5dafd58d5868bf5d28d82c5e2f3e2ecfb3ebe03a
Eduardo <eduardorobles@protonmail.com>
2024-10-09 05:54:02 -0500

    ADD: Adding function to presentation
    These functions are what drives the "demo" portion of the
    presentation. They include stuff on creating a container and running
    tools in inside of them.

--- talks-2024/mal_inv_containers_present.el
+++ talks-2024/mal_inv_containers_present.el
@@ -5,6 +5,27 @@
 ;;; Code:
 (require 'demo-it)
 
+;; Define functions for use as demo's in the slides
+;; (defun name/variable ())
+
+(defun mal-demo/pull-remnux ()
+  "pull REMnux Container"
+  (demo-it-run-in-shell "podman pull docker.io/remnux/remnux-distro:focal"))
+
+(defun mal-demo/run-remnux ()
+  "run REMnux Container"
+  (demo-it-run-in-shell "podman run --rm -it -u remnux remnux/remnux-distro:focal bash"))
+
+(defun mal-demo/init-clamscan ()
+  "initialize/update clamscan"
+  (demo-it-run-in-shell "freshclam")) ;; get updated command
+
+(defun mal-demo/run-clamscan ()
+  "run Clamscan on eml file"
+  (demo-it-show-shell "clamscan --debug /path/to/emailtoscan.eml")) ;; use demo-it-shart-shell and the function run-remnux before this function
+
+
+
 (demo-it-create :windows-below :fullscreen :advance-mode :use-shell :variable-width :insert-fast :text-large
 		(demo-it-presentation "mal_inv_containers.org")
 		demo-it-presentation-advance
